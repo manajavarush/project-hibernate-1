c1f2c5dd44c853e4bf57d26f8fdc2360a722cd6e
Stanislau Sinkevich <manamlmain@gmail.com>
2022-12-04 14:17:43 +0300

    Completed application

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,38 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.NamedQuery;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name = "player", schema = "rpg")
+@NamedQuery(name = "player_getAllCount", query = "select count(p) from Player p")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
+    @Column(nullable = false, length = 12)
     private String name;
 
+    @Column(nullable = false, length = 30)
     private String title;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Race race;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Profession profession;
 
+    @Column(nullable = false)
     private Date birthday;
-
+    @Column(nullable = false)
     private Boolean banned;
-
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
